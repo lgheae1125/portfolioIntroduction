7008718cdbec19848cbe5b509c1e0c8b478c8ec4
LGH <leeganghee4954@naver.com>
2025-04-15 19:46:58 +0900

    feat: fix background image

--- app/(providers)/(root)/page.tsx
+++ app/(providers)/(root)/page.tsx
@@ -1,7 +1,23 @@
 import React from "react";
 
-function Home() {
-  return <div>page</div>;
+function HomePage() {
+  return (
+    <main>
+      <div className="bg-[url('/images/backgroundIMG.jpg')] w-full h-screen fixed top-0 -z-10">
+        <h2 className="text-[10rem] text-white font-black">Front-End</h2>
+        <h2 className="text-5xl text-black font-medium">
+          프론트엔드 개발자{" "}
+          <span className="text-5xl text-black font-black">이강희</span>
+          입니다.
+        </h2>
+        <p>
+          사용자를 생각하는 역지사지 마인드, <br /> 포기하지 않고 책임감있게
+          일하는 개발자입니다.
+        </p>
+      </div>
+      <div className="h-[2000px] bg-black mt-[100vh]"></div>
+    </main>
+  );
 }
 
-export default Home;
+export default HomePage;
